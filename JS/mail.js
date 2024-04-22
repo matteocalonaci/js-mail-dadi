0d90e85ab7ea6245b45124a322681072ac0b3596
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-04-22 20:44:14 +0200

    commit 3

--- JS/mail.js
+++ JS/mail.js
@@ -1,5 +1,17 @@
 
-MAIL
+// MAIL
 
-Chiedi all’utente la sua email per poi controllare che sia nella lista di chi può accedere.
-Stampa un messaggio SOLO SE l'utente è in lista.
+// Chiedi all’utente la sua email per poi controllare che sia nella lista di chi può accedere.
+// Stampa un messaggio SOLO SE l'utente è in lista.
+
+let emailUtente = prompt("Inserisci la tua email");
+console.log("email utente:" + emailUtente);
+
+const emailList = [
+    "matteo.calonaci.1994@gmail.com",
+    "ugo@gmail.com",
+    "giorgione@gmail.com",
+];
+console.log("email utenti:" + emailList);
+
+
